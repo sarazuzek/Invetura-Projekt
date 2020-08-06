d216b135bcf41bcde8366e1a63a08d868918b081
sara.zuzek <sara.zuzek@gmail.com>
2020-08-06 14:36:17 +0200

    popravila model

--- model.py
+++ model.py
@@ -13,8 +13,8 @@ class Uporabnik:
     def shrani_stanje(self, ime_datoteke):
         slovar_stanja = {
             'uporabnisko_ime' : self.uporabnisko_ime,
-            'zasifrirano_geslo' : self.zasifrirano_geslo
-            'inventura' : self.inventura.slovar_s_stanjem()
+            'zasifrirano_geslo' : self.zasifrirano_geslo,
+            'inventura': self.inventura.slovar_s_stanjem()
         }
         with open(ime_datoteke, 'w', encoding='utf-8') as datoteka:
             json.dump(slovar_stanja, datoteka, ensure_ascii=False, indent=4)
@@ -46,12 +46,12 @@ class Inventura:
         if type(kolicina) != int:
             raise TypeError('Količina mora biti celo število!')
         if (kategorija, ime) in self.izdelki:
-            self.izdelki[(kategorija, ime)][2] += kolicina
+            self.izdelki[(kategorija, ime)][2] += kolicina           
         else:
-            self.izdelki[(kategorija, ime)] = (nabavna_cena, prodajna_cena, kolicina)
+            self.izdelki[(kategorija, ime)] = [nabavna_cena, prodajna_cena, kolicina]   #spremenila v niz, saj nabori niso delali
             #print('Dodajam novo kategorijo:', kategorija)
 
-    def izdelki_po_kategorijah(self):   
+    def izdelki_po_kategorijah(self):               
         kategorije = {}
         for k,v in self.izdelki.items():
             kategorija = k[0]
@@ -59,7 +59,10 @@ class Inventura:
             nabavna_cena = v[0]
             prodajna_cena = v[1]
             kolicina = v[2]
-            kategorije[kategorija] = (ime, nabavna_cena, prodajna_cena, kolicina)
+            if kategorija in kategorije:
+                kategorije[kategorija].append([ime, nabavna_cena, prodajna_cena, kolicina])
+            else:
+                kategorije[kategorija] = [[ime, nabavna_cena, prodajna_cena, kolicina]]
         return kategorije
 
     def odstrani_izdelek(self, kategorija, ime):
@@ -69,35 +72,39 @@ class Inventura:
             raise ValueError(f'{ime} ni v kategoriji {kategorija}.')
 
     def prenesi_izdelek(self, kategorija1, kategorija2, ime): #prenesli bomo iz kategorije1 v kategorijo2
-        kategorije = self.izdelki_po_kategorijah()
-        self._preveri_izdelek_v_kategoriji(ime, kategorija1, kategorije) #vrne error, če izdelka ni v kategoriji1
-        self._preveri_izdelek_v_kategoriji(ime, kategorija2, kategorije) #vrne error, če je izdelek že v kategoriji2
+        self._preveri_izdelek_v_kategoriji(ime, kategorija1) #vrne error, če izdelka ni v kategoriji1
+        self._preveri_izdelek_iz_kategorije(ime, kategorija2) #vrne error, če je izdelek že v kategoriji2
+        nabavna_cena = self.izdelki[(kategorija1, ime)][0]
+        prodajna_cena = self.izdelki[(kategorija1, ime)][1]
+        kolicina = self.izdelki[(kategorija1, ime)][2]
+        self.izdelki[(kategorija2, ime)] = [nabavna_cena, prodajna_cena, kolicina]  
         del self.izdelki[(kategorija1, ime)]
-        nabavna_cena = kategorije[kategorija1][1]
-        prodajna_cena = kategorije[kategorija1][2]
-        kolicina = kategorije[kategorija1][3]
-        self.izdelki[(kategorija2, ime)] = (nabavna_cena, prodajna_cena, kolicina)  
 
-    def _preveri_izdelek_v_kategoriji(self, ime, kategorija, kategorije):
-        if ime not in kategorije[kategorija]:
-            raise ValueError(f'{ime} ni/je že v kategoriji {kategorija}!')
+    def _preveri_izdelek_v_kategoriji(self, ime, kategorija):
+        if (kategorija, ime) not in self.izdelki:
+            raise ValueError(f'{ime} ni v kategoriji {kategorija}!')
+    
+    def _preveri_izdelek_iz_kategorije(self, ime, kategorija):
+        if (kategorija, ime) in self.izdelki:
+            raise ValueError(f'{ime} je že v kategoriji {kategorija}!')
 
     def dodaj_racun(self, kategorija, izdelek, kolicina, popust=0, prodaj_po_nabavni=False):
         self._preveri_stevilke(kolicina, popust)
         self._preveri_izdelek(kategorija, izdelek, kolicina) #vrne error če izdelk ni ali ga ni dovolj na zalogi
-        nabavna_cena = izdelki[(kategorija, izdelek)][0]
-        prodajna_cena = izdelki[(kategorija, izdelek)][1]
+        nabavna_cena = self.izdelki[(kategorija, izdelek)][0]
+        prodajna_cena = self.izdelki[(kategorija, izdelek)][1]
         if prodaj_po_nabavni:
             skupna_vrednost = kolicina * nabavna_cena
         else:
             skupna_vrednost = kolicina * prodajna_cena * (1 - popust / 100)
         if (kategorija, izdelek) in self.vsi_racuni:
-            self.vsi_racuni[(kategorija, izdelek)].append((kolicina, skupna_vrednost))
-        self.vsi_racuni[(kategorija, izdelek)] = [(kolicina, skupna_vrednost)]
-        self.izdelki[(kategorija, izdelek)][2] -= kolicina
+            self.vsi_racuni[(kategorija, izdelek)].append([kolicina, skupna_vrednost])
+        else:
+            self.vsi_racuni[(kategorija, izdelek)] = [[kolicina, skupna_vrednost]]
+        self.izdelki[(kategorija, izdelek)][2] -= kolicina    
 
     def _preveri_stevilke(self, kolicina, popust=0):
-        if type(popust) != int:
+        if type(popust) != int:                                  
             raise TypeError('Popust mora biti celo število!')
         if type(kolicina) != int:
             raise TypeError('Količina mora biti celo število večje ali enako 0!')
@@ -113,13 +120,15 @@ class Inventura:
         if kolicina > zaloga:
             raise ValueError(f'Ni dovolj zaloge za izdelek {izdelek}.')
 
-    def storniraj_racun(self, kategorija, izdelek, popravljna_kolicina, popravljena_popust=0):
+    def storniraj_racun(self, kategorija, izdelek, popravljna_kolicina, popravljen_popust=0):
         #ta funkcija popravi zadnji vnešen račun za izdelek v kategoriji (če se zmotiš pri vnosu)
+        stara_kolicina = self.vsi_racuni[(kategorija, izdelek)][-1][0]
         if (kategorija, izdelek) in self.vsi_racuni:
             self.vsi_racuni[(kategorija, izdelek)].pop()
         else:
             raise ValueError(f'Izdelka {izdelek} ni med računi!')
         self.dodaj_racun(kategorija, izdelek, popravljna_kolicina, popravljen_popust)
+        self.izdelki[(kategorija, izdelek)][2] += stara_kolicina
         
     def dodaj_inventuro(self, kategorija, izdelek, kolicina):
         self._preveri_izdelek(kategorija, izdelek) #preveri ali izdelek obstaja na seznamu vseh izdelkov
@@ -130,11 +139,11 @@ class Inventura:
         if (kategorija, izdelek) in self.inventura:
             raise ValueError(f'Izdelek {izdelek}, je že v inventuri!')
 
-     def sestej_kolicine(self, kategorija, izdelek):
+    def sestej_kolicine(self, kategorija, izdelek):    
         self._preveri_(kategorija, izdelek)
-        inventura = self.inventura[(kategorija, izdelek)]
+        inventura_izdelek = self.inventura[(kategorija, izdelek)][0]  #'int' object is not subscriptable
         na_zacetku = self.izdelki[(kategorija, izdelek)][2]
-        if invetura != na_zacetku:
+        if invetura_izdelek != na_zacetku:
             raise ValueError(f'Pri izdelku {izdelek} se nekaj ne ujema!')
     
     def _preveri_(self, kategorija, izdelek):
@@ -146,12 +155,13 @@ class Inventura:
             raise ValueError(f'Izdelka {izdelek} še ni v inventuri!')
 
     def dobicek_na_izdelku(self, kategorija, izdelek):
-        self._preveri_(kategorija, izdelek)
+        self._preveri_izdelek(kategorija, izdelek)
         prodaja = self._zasluzek_prodaja(kategorija, izdelek) #dobiček od prodaje izdelka
         nabavna_cena = self.izdelki[(kategorija, izdelek)][0]
         kolicina = self._kolicina_prodanih_izdelkov(kategorija, izdelek) #število prodanih izdelkov
         placano = kolicina * nabavna_cena 
         dobicek = prodaja - placano
+        return dobicek
 
     def _zasluzek_prodaja(self, kategorija, izdelek):
         vsota = 0
@@ -209,7 +219,7 @@ class Inventura:
     @classmethod 
     def nalozi_iz_slovarja(cls, slovar_s_stanjem):
         inventura = cls()
-        for izdelek in slovar_s_stanjem['vsi izdelki'][]:
+        for izdelek in slovar_s_stanjem['vsi izdelki']:
             nov_izdelek = inventura.dodaj_izdelek(
                 izdelek['kategorija'],
                 izdelek['ime'],
@@ -241,9 +251,6 @@ class Inventura:
         with open(ime_datoteke) as datoteka:
             slovar_s_stanjem = json.load(datoteka)
         return cls.nalozi_iz_slovarja(slovar_s_stanjem)
-
-
-
 
 
 
